351e8a2566044dc2eda4703ca4f4359132af928a
elvisonob <elvisonob@yahoo.com>
2024-07-18 19:29:34 +0100

    Solving Lists

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,35 +1,30 @@
 ﻿using System;
 
 namespace Coding.Exercise
-
-
 {
     public class Exercise
     {
         public List<string> GetOnlyUpperCaseWords(List<string> words)
         {
             //your code goes here
-            List<string> newArray = new List<string>();
-            foreach (var word in words)
+            List<string> result = new List<string>();
+            foreach(var word in words)
             {
-                foreach (char word2 in word)
+                bool isWordContains = true;
+foreach(var word2 in word)
                 {
                     if (char.IsUpper(word2))
                     {
-                        newArray.Add(word);
-                    };
+                        continue;
+                    }
+
+                    if (isWordContains)
+                    {
+                        result.Add(word);
+                    }
                 }
             }
-            {
-
-            }
-
-            //if two or more words are same and they are already caps, return one of them
-
-
-
-
+            return result;
         }
     }
 }
-
