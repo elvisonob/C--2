66a4b1cadb6936ab203a76ea2289f0c7b7921934
elvisonob <elvisonob@yahoo.com>
2024-07-24 18:38:29 +0100

    out method

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,22 +1,3 @@
-﻿var numbers = new[] { 10, -8, 2, 12, -17 };
+﻿Console.WriteLine("Enter a number:");
+var userInput = Console.ReadLine();
 
-var positiveNumbers = GetPositiveNumbers(numbers);
-
-foreach(var number in positiveNumbers)
-{
-    Console.WriteLine(number);
-}
-
-List<int> GetPositiveNumbers(int[] number)
-{
-    List<int> numberResult = new List<int>();
-    foreach(var numbers in number)
-    {
-        if (numbers > 0)
-        {
-            numberResult.Add(numbers);
-        }
-    }
-
-    return new List<int>(numberResult);
-}
