72588f25b2e29749b6a0f6ed85df3255856be203
elvisonob <elvisonob@yahoo.com>
2024-08-06 18:59:21 +0100

    TryParse method done

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,3 +1,17 @@
-﻿Console.WriteLine("Enter a number:");
-var userInput = Console.ReadLine();
+﻿bool isParsingSuccessful;
+do
+{
+    Console.WriteLine("Enter a number:");
+    var userInput = Console.ReadLine();
+    isParsingSuccessful = int.TryParse(userInput, out int number);
+    if (isParsingSuccessful)
+    {
+        Console.WriteLine("Parsing is successful " + number);
+    }
+    else
+    {
+        Console.WriteLine("Parsing was not successful");
+    }
+} while (!isParsingSuccessful);
 
+Console.ReadKey();
